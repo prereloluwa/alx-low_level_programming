5e35c500c0e060a36e7475d9cac5cf03f47cf42f
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-09-11 13:26:31 +0000

    a program that assigns a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,19 +24,19 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 
 {
-printf("%d is positive ", n);
+printf("%lc is positive ", n);
 }
 
 else if (n < 0)
 
 {
-printf("%d is negative ", n);
+printf("%lc is negative ", n);
 }
 
 else
 
 {
-printf("%d is zero ", n);
+printf("%lc is zero ", n);
 }
 
 return (0);
