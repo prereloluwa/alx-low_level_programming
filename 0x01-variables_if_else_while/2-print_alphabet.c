86ae91a1b308cd2606a94704a5366aafb382ce74
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-09-12 01:10:37 +0000

    a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Always)
+ */
+
+int main(void)
+{
+
+char a2z;
+
+for (a2z = 'a' ; a2z <= 'z' ; a2z++)
+{
+putchar (a2z);
+}
+{
+putchar ('\n');
+}
+return (0);
+}
