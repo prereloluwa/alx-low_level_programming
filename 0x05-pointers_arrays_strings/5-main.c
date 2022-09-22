cd48f147f96d30a4828793745326c95f4e388f10
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-09-22 08:10:00 +0000

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+
+#include main.h
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[10] = My School;
+
+    printf(%sn, s);
+    rev_string(s);
+    printf(%sn, s);
+    return (0);
+}
