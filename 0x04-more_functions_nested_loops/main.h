9ff34584590ff520f2dac5d292a809640215b011
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-09-29 21:33:33 +0000

    modified

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MIAN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
-void print_numbers(void);
+//void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
