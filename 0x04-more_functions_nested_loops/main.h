9806c328e5c2d04e4fa142c569166a46314f0aad
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-09-29 21:43:12 +0000

    modified

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MIAN_H
 
 int _putchar(char c);
+void print_number(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,6 +13,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 
 #endif
