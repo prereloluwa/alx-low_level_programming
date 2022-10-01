06fb3e2cccd7d7d7a75e8b9cbb19b452e305af09
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-10-01 19:53:18 +0000

    a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function to that takes in two declarations
+ *
+ * @argc: indexing argument
+ * @argv: argument that tkes in external parameters
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+
+	printf("%d\n", argc - 1);
+	
+	return 0;
+}
