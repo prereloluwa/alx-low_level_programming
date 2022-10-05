db8ac240e2521162c6d99312aef4103f58185ed1
Philip R. ISHOLA <ishola.philip@hotmail.com>
2022-10-05 04:58:12 +0000

    a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,44 +1,52 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * *argstostr - concatenates all the arguments of the program
- * @ac: number of arguments
- * @av: array of arguments
- * Return: Pointer to the new string (Success), NULL (Error)
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
  */
 char *argstostr(int ac, char **av)
 {
-	int a, j, k, len;
-	char *str;
+	char *aout;
+	int c, i, j, ia;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
 
-	for (a = 0; a < ac; a++)
+	for (c = i = 0; i < ac; i++)
 	{
+		if (av[i] == NULL)
+			return (NULL);
+
 		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+			c++;
+		c++;
 	}
 
-	str = malloc(sizeof(char) * (len + 1));
+	aout = malloc((c + 1) * sizeof(char));
 
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (a = 0; a < ac; a++)
+	if (aout == NULL)
 	{
-		for (j = 0; av[a][j] != '\0'; j++)
-		{
-			str[k] = av[a][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
+		free(aout);
+		return (NULL);
 	}
 
-	return (str);
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
 }
