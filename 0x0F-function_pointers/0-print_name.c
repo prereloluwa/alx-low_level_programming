c3afe27f8f662e17ffc92a610fc7fb84ca848053
Philip ISHOLA <ishola.philip@hotmail.com>
2022-10-14 16:47:31 +0100

    Function pointers main tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,12 @@
  * print_name - function to print name
  * @name: char pointer to a function name
  * @f: woid pointer to a function f
- * Return:
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
