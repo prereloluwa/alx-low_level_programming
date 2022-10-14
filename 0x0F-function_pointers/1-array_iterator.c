aca5acd67196812688de79bafbd9dd2136a56019
Philip ISHOLA <ishola.philip@hotmail.com>
2022-10-14 17:00:17 +0100

    Function pointers main tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
